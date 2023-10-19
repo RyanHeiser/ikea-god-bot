d136b2d0cfd7ae49723b7e73fc064b9d793bf3b0
BuildTools <unconfigured@null.spigotmc.org>
2023-10-18 18:10:01 -0700

    tts

---
+++ SpeechSynthesis.js
@@ -0,0 +1,51 @@
+const fs = require('node:fs');
+
+function xmlToString(filePath) {
+    const xml = fs.readFileSync(filePath, "utf8");
+    return xml;
+}
+
+function speechSynthesis(text) {
+    return new Promise((resolve, reject) => {
+
+        "use strict";
+
+        var sdk = require("microsoft-cognitiveservices-speech-sdk");
+    
+        var audioFile = "./audio/speech.wav";
+        // This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+        const speechConfig = sdk.SpeechConfig.fromSubscription(process.env.SPEECH_KEY, process.env.SPEECH_REGION);
+        const audioConfig = sdk.AudioConfig.fromAudioFileOutput(audioFile);
+    
+        // The language of the voice that speaks.
+        speechConfig.speechSynthesisVoiceName = "sv-SE-MattiasNeural"; 
+        
+        // Create the speech synthesizer.
+        var synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
+
+        var ssml = xmlToString('./speech.xml');
+        ssml = ssml.replace("sample text", text);
+        synthesizer.speakSsmlAsync(ssml,
+            function (result) {
+        if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
+            console.log("synthesis finished.");
+            resolve();
+        } else {
+            console.error("Speech synthesis canceled, " + result.errorDetails +
+                "\nDid you set the speech resource key and region values?");
+            reject();
+        }
+        synthesizer.close();
+        synthesizer = null;
+        },
+            function (err) {
+        console.trace("err - " + err);
+        synthesizer.close();
+        synthesizer = null;
+        });
+        console.log("Now synthesizing to: " + audioFile);
+    });
+}
+  
+  // Export the function as a module
+  module.exports = speechSynthesis;
--- main.js
+++ main.js
@@ -1,10 +1,59 @@
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const path = require('node:path');
 const fs = require('node:fs');
+const express = require('express');
+const { port } = require('./config.json');
 require("dotenv").config();
 
-const client = new Client({ intents: [GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildVoiceStates] });
 
+client.commands = new Collection();
+
+/*
+// Command Handler
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+// Command Executor
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+*/
+
+// Events Handler
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
@@ -17,5 +66,17 @@ for (const file of eventFiles) {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
 }
+
+
+
+// REDIRECT
+const app = express();
+
+app.get('/', (request, response) => {
+	return response.sendFile('index.html', { root: '.' });
+});
+
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
+
 
 client.login(process.env.DISCORD_KEY);
